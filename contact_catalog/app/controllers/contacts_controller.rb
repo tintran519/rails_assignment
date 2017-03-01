8b064318ceb577c870dd4f59a3351e06232ba370
Tin Tran <tin.tran0519@gmail.com>
2017-02-28 23:03:00 -0800

    show and new routes created

--- contact_catalog/app/controllers/contacts_controller.rb
+++ contact_catalog/app/controllers/contacts_controller.rb
@@ -2,4 +2,12 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
   end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+  end
 end
--- contact_catalog/config/routes.rb
+++ contact_catalog/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get "contacts/", to: "contacts#index"
+  get "contacts/new", to: "contacts#new"
+  get "contacts/:id", to: "contacts#show"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
